79699c5db797f867d47d622678afa58158ad5475
Rakesh Sirvi <rakusirvi797@gmail.com>
2023-02-13 15:08:07 +0530

    updated news api

--- app/adapters/news.js
+++ app/adapters/news.js
@@ -1,15 +1,23 @@
 import RESTAdapter from '@ember-data/adapter/rest';
 
 export default class NewsAdapter extends RESTAdapter {
-  host = 'https://bing-news-search1.p.rapidapi.com';
-
-  headers = {
-    'X-BingApis-SDK': 'true',
-    'X-RapidAPI-Key': '7dcb741adamsh9a1e580d8d291f7p180c0bjsn7766bf702906',
-    'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com',
-  };
+  host = 'https://api.thenewsapi.com/v1';
 
   buildURL() {
-    return `${this.host}/news`;
+    return `${this.host}/news/top?api_token=fNf30qoyqLU4rSDVBpHXyb4BHQl11bgCrnNdA9gf&language=en`;
+  }
+
+  query(_, __, query) {
+    if (localStorage.getItem(`news.${query.locale}`)) {
+      return Promise.resolve(
+        JSON.parse(localStorage.getItem(`news.${query.locale}`))
+      );
+    }
+    const data = super.query(...arguments).then((_data) => {
+      localStorage.setItem(`news.${query.locale}`, JSON.stringify(_data));
+      return _data;
+    });
+
+    return data;
   }
 }
--- app/serializers/news.js
+++ app/serializers/news.js
@@ -2,17 +2,15 @@ import RESTSerializer from '@ember-data/serializer/rest';
 
 export default class NewsSerializer extends RESTSerializer {
   normalizeResponse(store, model, payload, ...rest) {
-    const news = payload.value.map((data, idx) => ({
+    const news = payload.data.map((data, idx) => ({
       id: 'news_' + idx,
-      headline: data.name,
+      headline: data.title,
       description: data.description,
-      image: data.image
-        ? `${data.image.thumbnail.contentUrl}&w=${data.image.thumbnail.width}&h=${data.image.thumbnail.height}`
-        : '',
+      image: data.image_url,
       url: data.url,
-      providerName: data.provider[0].name,
-      providerImage: data.provider[0].image?.thumbnail.contentUrl ?? '',
-      published: data.datePublished,
+      providerName: data.source,
+      providerImage: '',
+      published: data.published_at,
     }));
 
     return super.normalizeResponse(store, model, { news }, ...rest);
